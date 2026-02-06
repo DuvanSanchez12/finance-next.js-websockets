005bfb11450a17ef934f777d87f40b2030cd6842
DuvanSanchez12 <duvandcrs@gmail.com>
2026-02-05 22:14:25 -0500

    Cambio en iconos cryptos

--- src/utils/img.tsx
+++ src/utils/img.tsx
@@ -3,5 +3,12 @@ export const getLogoUrl = (asset: any) => {
     const rawSymbol = asset.symbol.includes(':') ? asset.symbol.split(':')[1] : asset.symbol;
     const baseSymbol = rawSymbol.replace('USDT', '').toUpperCase();
     // console.log("Base Symbol:", baseSymbol, "from Raw Symbol:", rawSymbol, `https://bin.bnbstatic.com/static/assets/logos/${baseSymbol}.png`);
-    return `https://bin.bnbstatic.com/static/assets/logos/${baseSymbol}.png`;
+    if (asset.type === 'crypto') {
+      // Fuente oficial de Binance
+      return `https://bin.bnbstatic.com/static/assets/logos/${baseSymbol}.png`;
+    } else {
+      // Fuente profesional para Stocks
+      const brandName = asset.name.split(' ')[0].toLowerCase();
+      return `https://bin.bnbstatic.com/static/assets/logos/${baseSymbol}.png`;
+    }
   };
